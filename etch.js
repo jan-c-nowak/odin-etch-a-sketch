142287d0ffb542a01e51f31cd8c1f03a461dc38c
jan-c-nowak <84571576+jan-c-nowak@users.noreply.github.com>
2024-04-17 22:32:32 +0200

    add building board

--- etch.js
+++ etch.js
@@ -2,20 +2,25 @@ function calculateGridElementSize(gridSideLength) {
     return (1/gridSideLength)*100; //returns in % container width
 }
 
+function styleGridElement(gridElement, relativeGridElemWidth){
+    gridElement.setAttribute('class', "grid-element");
+    gridElement.style.flexBasis = `${relativeGridElemWidth}%`;
+    gridElement.style.height = `${relativeGridElemWidth}%`;
+}
+
 function generateSquareGrid(gridSideLength){
     const container = document.querySelector(".container");
     const relativeGridElemWidth = calculateGridElementSize(gridSideLength) - 0.2; //0.2 comes from grid-element 0.1% margin 
 
     for(let i=0; i < gridSideLength * gridSideLength; i++){
         let gridElement = document.createElement("div");
-        gridElement.setAttribute('class', "grid-element");
 
-        
-        gridElement.style.flexBasis = `${relativeGridElemWidth}%`;
-        gridElement.style.height = `${relativeGridElemWidth}%`;
+        styleGridElement(gridElement,relativeGridElemWidth);
         
         container.appendChild(gridElement);
     }
 }
 
-generateSquareGrid(8);
+
+
+generateSquareGrid(16);
